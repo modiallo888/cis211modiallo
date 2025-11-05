02ee395e13ef57bf9c599048146c19233be219a3
modiallo888 <md0456999@gmail.com>
2025-11-05 10:35:26 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd 
+from datetime import datetime
+
+# Page Config
+st.set_page_config(
+  page_title ='Avinash Jairam | Portfolio',
+  page_icon='🎯',
+  layout = 'wide'
+)
+
+# Custom CSS (optional - for styling)
+st.markdown('''
+                <style>
+                    .main-header {font-size: 42px; font-weight: bold; text-align:center;}
+                    .sub-header {font-size: 24px; text-align:center; color: #666;}
+                </style>
+            ''', unsafe_allow_html = True)
+
+# Sidebar
+st.sidebar.title('📍 Navigation')
+page = st.sidebar.radio('Go to',
+                        ['🏠 Home', '🤠 About', ' 💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
+
+# Home Page
+if page == '🏠 Home':
+  st.markdown('<p class="main-header">Avinash Jairam</p>', unsafe_allow_html=True)
+  st.markdown('<p class="sub-header">Aspiring Tech Professional | Medgar Evers College</p>', unsafe_allow_html=True)
+
+  # Three Columns for stats
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+      st.metric('GPA', '3.8', '📚')
+  with col2:
+      st.metric('Projects', '5', '💻')
+  with col3:
+      st.metric('Skills', '10+', '🚀')
+
+  st.write('---')
+
+  # Introduction with columns
+  col1, col2 = st.columns([2,1])
+  with col1:
+    st.subheader('Welcome to my digital space!👋')
+    st.write('''
+                I am a Computer Information Systems student passionate about web development and emerging technologies. Currently learning
+                HTML, CSS, JavaScript, and Python to build innovative solutions.
+            
+                🎯 **Current Focus:** Building interactive web applications with Streamlit
+            
+                📚 **Currently Learning:** Internet and Emergin Technologies (CIS 211)
+            
+                🌱 **Fun Fact:** I can solve a Rubik's cube in under 2 minutes!
+            ''')
+
+
+
+
+
+
+
+
+
+
+
