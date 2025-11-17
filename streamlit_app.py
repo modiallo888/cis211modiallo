7031992c2b27816518f171d0cfa7bcfed00e2ac7
modiallo888 <md0456999@gmail.com>
2025-11-17 10:36:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # Page Config
 st.set_page_config(
-  page_title 'Mohamed Diallo | Portfolio',
+  page_title ='Avinash Jairam | Portfolio',
   page_icon='🎯',
   layout = 'wide'
 )
@@ -20,7 +20,7 @@ st.markdown('''
 # Sidebar
 st.sidebar.title('📍 Navigation')
 page = st.sidebar.radio('Go to',
-                        ['🏠 Home', '🤠 About', ' 💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
+                        ['🏠 Home', '🤠 About', '💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
 
 # Home Page
 if page == '🏠 Home':
@@ -53,14 +53,128 @@ if page == '🏠 Home':
             
                 🌱 **Fun Fact:** I can solve a Rubik's cube in under 2 minutes!
             ''')
+  with col2:
+    # Placeholder for image
+    st.image('https://raw.githubusercontent.com/avinashjairam/cis211_project1/refs/heads/main/grumpy_cat.jfif', use_column_width=True)
+
+# About Page
+elif page == '🤠 About':
+  st.title('About Me')
+
+  # Timeline of my Professional Journey
+  st.subheader('My Journey 🗺️')
+
+  with st.expander('2025 - Present: Medgar Evers College'):
+    st.write('''
+                - Major: Computer Information Systems
+                - Relevant Coursework: Internet & Emerging Technologies, Programming, Database Systems, AI
+                - Activities: Track Team, Volleyball Team, Hackathon participant
+            ''')
+
+  with st.expander('2023 - 2025: NYC Museum School'):
+    st.write('''
+                - Graduated with honors
+                - AP Computer Science A (Score: 5)
+                - Founded Coding Club
+            ''')
+
+  st.subheader('Interests & Hobbies 🏀')
+  interests = ['Web Development', 'AI/Machine Learning', 'Photography', 'Basketball', 'Travel', 'Baseball']
+
+  # Display the interests in columns
+  cols = st.columns(3)
+  for i, interest in enumerate(interests):
+    with cols[i % 3]:
+      st.info(f'🔷 {interest}')
+      
+elif page == '💼 Projects':
+  st.title('My Projects')
+  st.write('Here are some projects I have worked on:')
+
+  # Project 1
+  with st.container():
+    col1, col2 = st.columns([1, 2])
+  
+    with col1:
+        st.image('https://iprx-cms-content.ams1.vultrobjects.com/Blog_How_To_Crawl_4_capcha_ded9206d5f.png', use_column_width = True)
+
+    with col2:
+        st.subheader('🛒 E-Commerce Price Tracker')
+        st.write('Python web scraper that monitors Amazon prices and sends alerts')
+        st.caption('**Technologies:** Python, BeautifulSoup, Streamlit')
+
+
+  # Project 2 
+  with st.container():
+    col1, col2 = st.columns([1,2])
+    with col1:
+      st.image('https://www.publicdomainpictures.net/pictures/90000/nahled/calculator-black-clipart.jpg', use_column_width = True)
+    with col2:
+      st.subheader('📊 Student Grade Calulator')
+      st.write('Interactive web app for calculating and visualizing grades')
+      st.caption('**Technologies:** Python, Pandas, Plotly')
+
+elif page == '🛠 Skills':
+  st.title('Technical Skills')
+
+  # Skills with progress bars
+  st.subheader('Programming Languages')
+
+  skills_data = {
+    'Python' : 85,
+    'HTML/CSS' : 70,
+    'JavaScript' : 60,
+    'SQL' : 50,
+    'Technical Writing' : 40
+  }
+
+  for skill, level in skills_data.items():
+    col1, col2 = st.columns([1,3])
+    with col1:
+      st.write(skill)
+    with col2:
+      st.progress(level/100)
+
+  st.subheader('Tools & Technologies')
+
+  col1, col2, col3 = st.columns(3)
+  with col1:
+    st.success('Excel')
+    st.info('Word')
+    st.warning('Access')
+
+  with col2:
+    st.success('PowerPoint')
+    st.info('Google Docs')
+    st.warning('ChatGPT/AI Tools')
+    
+  with col3:
+    st.success('Presentations')
+    st.info('Writing')
+    st.warning('Social Media')
+
+elif page == '📝 Resume':
+  st.title('Resume')
+
+  # Read PDF from my GitHub repository
+  with open('my_resume.pdf', 'rb') as pdf_file:
+    PDFbyte = pdf_file.read()
+  
+  st.download_button(
+    label ='🔻 Download Full Resume (PDF)',
+    data = PDFbyte,
+    file_name = 'my_resume.pdf',
+    mime ='application/pdf'
+  )
+
+elif page == '📩 Contact':
+  st.title("Let's Connect!")
+
+  col1, col2 = st.columns(2)
+
+  with col1:
+    st.subheader('Send me a message.')
 
 
 
-
-
-
-
-
-
-
-
+      
