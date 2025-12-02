6e4cad218c82a5ab739d21f85ec6a6c200bc9d75
modiallo888 <md0456999@gmail.com>
2025-12-01 23:40:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,9 +139,9 @@ elif page == '🛠 Skills':
 
   col1, col2, col3 = st.columns(3)
   with col1:
-    st.success('Excel')
+    st.success('Instagram')
     st.info('Word')
-    st.warning('Access')
+    st.warning('Meta')
 
   with col2:
     st.success('PowerPoint')
